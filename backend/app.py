f7dd495673153de9325f9a312dcb3e0a044d4a47
Arav <budhiraja.ara@northeastern.edu>
2025-11-11 18:43:54 -0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -15,7 +15,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://memory-machines-arav.vercel.app/"],
+    allow_origins=["https://memory-machines-arav.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
